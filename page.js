2bc39cbbb097e17695f7bc79d1a68282f45b62c0
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-19 17:57:46 -0500

    Quotes array
    Created an array to hold all the quotes.

---
+++ page.js
@@ -0,0 +1,10 @@
+let quotes = [
+  {
+    quote: "This class is front-end development",
+    author: "Eric York"
+  },
+  {
+    quote: "I am taking this class",
+    author: "Chastant Shontz"
+  }
+];
