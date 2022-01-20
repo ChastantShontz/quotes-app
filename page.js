8a325d461d68a9e1f8314da968ee05ced75b07dd
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-19 20:35:23 -0500

    Select quote
    Set the variable index to 0.
    
    Used the function selectQuote to set the inner HTML of the paragraph #quote to the first quote in the array using the index.

--- page.js
+++ page.js
@@ -2,9 +2,14 @@ let quotes = [
   {
     quote: "This class is front-end development",
     author: "Eric York"
-  },
-  {
+  }, {
     quote: "I am taking this class",
     author: "Chastant Shontz"
   }
 ];
+
+let index = 0;
+
+function selectQuote () {
+  document.getElementById("quote").innerHTML = quotes[index].quote;
+};
