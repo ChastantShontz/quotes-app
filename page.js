ced571c8dd633648c3ced5ff801dcdccadae9756
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-21 15:57:35 -0500

    Load background colors
    Set the session storage equal to a random number in between 1 and 6.
    
    Let the variable color equal the session storage.
    
    Assigned a different color value to each number.

--- page.js
+++ page.js
@@ -13,6 +13,27 @@ let index = sessionStorage.index;
 document.getElementById("quote").innerHTML = quotes[index].quote;
 document.getElementById("author").innerHTML = quotes[index].author;
 
+sessionStorage.color = Math.ceil(Math.random() * 6);
+let color = sessionStorage.color;
+if (color == 1) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--red)";
+}
+else if (color == 2) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--orange)";
+}
+else if (color == 3) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--yellow)";
+}
+else if (color == 4) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--green)";
+}
+else if (color == 5) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--blue)";
+}
+else if (color == 6) {
+  document.getElementById("quotesCont").style.backgroundColor = "var(--purple)";
+}
+
 function forward() {
   if (sessionStorage.index == quotes.length - 1) {
     sessionStorage.index = 0;
