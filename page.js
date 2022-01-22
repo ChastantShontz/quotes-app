e00a6ddce11e009f4b124ef4f79e17af1759e06c
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-21 19:14:27 -0500

    Switch background colors
    Created the function selectColor and put it in the functions forward and back.
    
    Set the session storage of color to a random number in between 1 and 6.
    
    This function increases color by a random number in between 1 and 5 to ensure that it never picks the same random number twice in a row.
    
    If color is greater than 6, it generates a new random number in between 1 and 6.

--- page.js
+++ page.js
@@ -9,57 +9,100 @@ let quotes = [
 ];
 
 sessionStorage.index = Math.floor(Math.random() * quotes.length);
-let index = sessionStorage.index;
-document.getElementById("quote").innerHTML = quotes[index].quote;
-document.getElementById("author").innerHTML = quotes[index].author;
+document.getElementById("quote").innerHTML = quotes[sessionStorage.index].quote;
+document.getElementById("author").innerHTML = quotes[sessionStorage.index].author;
 
 sessionStorage.color = Math.ceil(Math.random() * 6);
-let color = sessionStorage.color;
-if (color == 1) {
+if (sessionStorage.color == 1) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--red)";
 }
-else if (color == 2) {
+else if (sessionStorage.color == 2) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--orange)";
 }
-else if (color == 3) {
+else if (sessionStorage.color == 3) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--yellow)";
 }
-else if (color == 4) {
+else if (sessionStorage.color == 4) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--green)";
 }
-else if (color == 5) {
+else if (sessionStorage.color == 5) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--blue)";
 }
-else if (color == 6) {
+else if (sessionStorage.color == 6) {
   document.getElementById("quotesCont").style.backgroundColor = "var(--purple)";
 }
 
 function forward() {
   if (sessionStorage.index == quotes.length - 1) {
     sessionStorage.index = 0;
-    let index = sessionStorage.index;
-    document.getElementById("quote").innerHTML = quotes[index].quote;
-    document.getElementById("author").innerHTML = quotes[index].author;
+    document.getElementById("quote").innerHTML = quotes[sessionStorage.index].quote;
+    document.getElementById("author").innerHTML = quotes[sessionStorage.index].author;
   }
   else {
     sessionStorage.index++;
-    let index = sessionStorage.index;
-    document.getElementById("quote").innerHTML = quotes[index].quote;
-    document.getElementById("author").innerHTML = quotes[index].author;
+    document.getElementById("quote").innerHTML = quotes[sessionStorage.index].quote;
+    document.getElementById("author").innerHTML = quotes[sessionStorage.index].author;
   }
+  selectColor();
 }
 
 function back() {
   if (sessionStorage.index == 0) {
     sessionStorage.index = quotes.length - 1;
-    let index = sessionStorage.index;
-    document.getElementById("quote").innerHTML = quotes[index].quote;
-    document.getElementById("author").innerHTML = quotes[index].author;
+    document.getElementById("quote").innerHTML = quotes[sessionStorage.index].quote;
+    document.getElementById("author").innerHTML = quotes[sessionStorage.index].author;
   }
   else {
     sessionStorage.index--;
-    let index = sessionStorage.index;
-    document.getElementById("quote").innerHTML = quotes[index].quote;
-    document.getElementById("author").innerHTML = quotes[index].author;
+    document.getElementById("quote").innerHTML = quotes[sessionStorage.index].quote;
+    document.getElementById("author").innerHTML = quotes[sessionStorage.index].author;
+  }
+  selectColor();
+}
+
+function selectColor() {
+  let color = +sessionStorage.color + Math.ceil(Math.random() * 5);
+  if (color > 6) {
+    let color = Math.ceil(Math.random() * 6);
+    sessionStorage.color = color;
+    if (sessionStorage.color == 1) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--red)";
+    }
+    else if (sessionStorage.color == 2) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--orange)";
+    }
+    else if (sessionStorage.color == 3) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--yellow)";
+    }
+    else if (sessionStorage.color == 4) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--green)";
+    }
+    else if (sessionStorage.color == 5) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--blue)";
+    }
+    else if (sessionStorage.color == 6) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--purple)";
+    }
+  }
+  else {
+    sessionStorage.color = color;
+    if (sessionStorage.color == 1) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--red)";
+    }
+    else if (sessionStorage.color == 2) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--orange)";
+    }
+    else if (sessionStorage.color == 3) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--yellow)";
+    }
+    else if (sessionStorage.color == 4) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--green)";
+    }
+    else if (sessionStorage.color == 5) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--blue)";
+    }
+    else if (sessionStorage.color == 6) {
+      document.getElementById("quotesCont").style.backgroundColor = "var(--purple)";
+    }
   }
 }
