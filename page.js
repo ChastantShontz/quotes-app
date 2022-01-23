95bcdcb8c34310f110503ae33a201521af8b29ba
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-23 14:19:37 -0500

    Quote array
    Added more quotes to the array.

--- page.js
+++ page.js
@@ -1,10 +1,145 @@
 let quotes = [
   {
-    quote: "This class is front-end development",
-    author: "Eric York"
+    quote: "At last, we will reveal ourselves to the Jedi.  At last, we will have revenge",
+    author: "Darth Maul"
   }, {
-    quote: "I am taking this class",
-    author: "Chastant Shontz"
+    quote: "Fear leads to anger.  Anger leads to hate.  Hate leads to suffering",
+    author: "Yoda"
+  }, {
+    quote: "Always two there are.  No more, no less.  A master and an apprentice",
+    author: "Yoda"
+  }, {
+    quote: "I'm just a simple man trying to make my way in the universe",
+    author: "Jango Fett"
+  }, {
+    quote: "The shroud of the dark side has fallen.  Begun, the Clone War has",
+    author: "Yoda"
+  }, {
+    quote: "In my book, experience outranks everything",
+    author: "Captain Rex"
+  }, {
+    quote: "You're reckless, little one.  You never would have made it as Obi-Wan's Padawan.  But you might make it as mine",
+    author: "Anakin Skywalker"
+  }, {
+    quote: "Today, we fight for more than the Republic.  Today, we fight for all our brothers back home",
+    author: "Captain Rex"
+  }, {
+    quote: "Had you said the word, I would have left the Jedi Order",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "You are the Chosen One.  You have brought balance to this world.  Stay on this path, and you will do it again for the galaxy.  But beware your heart...",
+    author: "The Father"
+  }, {
+    quote: "We are loyal soldiers, we follow orders, but we are not a bunch of unthinking droids!  We are men!  We must be trusted to make the right decision, especially when the orders we are given are wrong",
+    author: "Fives"
+  }, {
+    quote: "You can kill me, but you will never destroy me.  It takes strength to resist the dark side.  Only the weak embrace it",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "There is no mercy",
+    author: "Darth Sidious"
+  }, {
+    quote: "My master abandoned me, and that's exactly what you've done to her.  You and your precious Jedi Order",
+    author: "Asajj Ventress"
+  }, {
+    quote: "The mission...the nightmares...they're finally over...",
+    author: "Fives"
+  }, {
+    quote: "No longer certain that one ever does win a war, I am.  For in fighting the battles, the bloodshed, already lost, we have",
+    author: "Yoda"
+  }, {
+    quote: "It is not the way of your people to hide here, in the gutters.  If you die, I promise you itt will be on the field of battle.  And if you die, you will die as warriors",
+    author: "Darth Maul"
+  }, {
+    quote: "Justice is merely a construct of the current power base",
+    author: "Darth Maul"
+  }, {
+    quote: "You're a good soldier, Rex.  So is every one of those men down there.  They may be willing to die, but I am not the one who is going to kill them",
+    author: "Ahsoka Tano"
+  }, {
+    quote: "Anakin, if one is to understand the great mystery, one must study all its aspects, not just the dogmatic, narrow view of the Jedi.  If you wish to become a complete and wise leader, you must embrace a larger view of the Force",
+    author: "Darth Sidious"
+  }, {
+    quote: "Once more, the Sith will rule the galaxy.  And we shall have peace",
+    author: "Darth Sidious"
+  }, {
+    quote: "So this is how liberty dies:  with thunderous applause",
+    author: "Padme Amidala"
+  }, {
+    quote: "You were my brother, Anakin!  I loved you",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "I was a coward.  But now, I know there's something stronger than fear, far stronger:  the Force.  Let me show you how strong it is",
+    author: "Kanan Jarrus"
+  }, {
+    quote: "Boys, this might be it.  At least we'll go down fighting like a clone should",
+    author: "Captain Rex"
+  }, {
+    quote: "I am no Jedi",
+    author: "Ahsoka Tano"
+  }, {
+    quote: "Battles leave scars.  Some you can't see",
+    author: "Kanan Jarrus"
+  }, {
+    quote: "To defeat an enemy, you must know them.  Not simply their battle tactics but their history, philosophy, art",
+    author: "Grand Admiral Thrawn"
+  }, {
+    quote: "The Empire wanted to destroy worlds, and they did.  They destroyed mine",
+    author: "Sabine Wren"
+  }, {
+    quote: "If you define yourself by the power to take life, the desire to dominate, to possess...then you have nothing",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "In my experience, when you think you understand the Force, you realize just how little you know",
+    author: "Ahsoka Tano"
+  }, {
+    quote: "Who deserves what is irrelevant.  What matters is who has power",
+    author: "Grand Admiral Thrawn"
+  }, {
+    quote: "We have hope.  Rebellions are built on hope",
+    author: "Jyn Erso"
+  }, {
+    quote: "Help me, Obi-Wan Kenobi.  You're my only hope",
+    author: "Princess Leia Organa"
+  }, {
+    quote: "For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic.  Before the dark times.  Before the Empire",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "Don't be too proud of this technological terror you've constructed.  The ability to destroy a planet is insignificant next to the power of the Force",
+    author: "Darth Vader"
+  }, {
+    quote: "You can't win, Darth.  If you strike me down, I shall become more powerful than you can possibly imagine",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "Remember:  the Force will be with you...always",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "Do, or do not.  There is no try",
+    author: "Yoda"
+  }, {
+    quote: "Luke, you're going to find that many of the truths we cling to depend greatly on our own point of view",
+    author: "Obi-Wan Kenobi"
+  }, {
+    quote: "The Force is strong in my family.  My father has it...I have it...and my sister has it",
+    author: "Luke Skywalker"
+  }, {
+    quote: "I am a Jedi...like my father before me",
+    author: "Luke Skywalker"
+  }, {
+    quote: "I can bring you in warm, or I can bring you in cold",
+    author: "Din Djarin"
+  }, {
+    quote: "I'm a Mandalorian.  Weapons are part of my religion",
+    author: "Din Djarin"
+  }, {
+    quote: "Everybody's got their line they won't cross until things get messy.  As far as I'm concrened, if you can make it through your day and still sleep at night, you're doing better than most",
+    author: "Migs Mayfeld"
+  }, {
+    quote: "You have something I want.  You may think you have some idea of what you are in possession of, but you do not.  Soon, he will be back with me.  He means more to me than you will ever know",
+    author: "Din Djarin"
+  }, {
+    quote: "He is strong with the Force, but talent without training is nothing",
+    author: "Luke Skywalker"
   }
 ];
 
