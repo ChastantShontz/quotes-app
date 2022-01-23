73ba4b6d693654f21049f4f6ceb2eb1dfec318b7
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2022-01-23 15:17:21 -0500

    Sources
    Added the source media of each quote to the text of #author.

--- page.js
+++ page.js
@@ -1,145 +1,148 @@
 let quotes = [
   {
     quote: "At last, we will reveal ourselves to the Jedi.  At last, we will have revenge",
-    author: "Darth Maul"
+    author: "Darth Maul<br><em>Star Wars:  Episode &#8544; &#8212; The Phantom Menace</em>"
   }, {
     quote: "Fear leads to anger.  Anger leads to hate.  Hate leads to suffering",
-    author: "Yoda"
+    author: "Yoda<br><em>Star Wars:  Episode &#8544; &#8212; The Phantom Menace</em>"
   }, {
     quote: "Always two there are.  No more, no less.  A master and an apprentice",
-    author: "Yoda"
+    author: "Yoda<br><em>Star Wars:  Episode &#8544; &#8212; The Phantom Menace</em>"
   }, {
     quote: "I'm just a simple man trying to make my way in the universe",
-    author: "Jango Fett"
+    author: "Jango Fett<br><em>Star Wars:  Episode &#8545; &#8212; Attack of the Clones</em>"
   }, {
     quote: "The shroud of the dark side has fallen.  Begun, the Clone War has",
-    author: "Yoda"
+    author: "Yoda<br><em>Star Wars:  Episode &#8545; &#8212; Attack of the Clones</em>"
   }, {
     quote: "In my book, experience outranks everything",
-    author: "Captain Rex"
+    author: "Captain Rex<br><em>Star Wars:  The Clone Wars</em>"
   }, {
     quote: "You're reckless, little one.  You never would have made it as Obi-Wan's Padawan.  But you might make it as mine",
-    author: "Anakin Skywalker"
+    author: "Anakin Skywalker<br><em>Star Wars:  The Clone Wars</em>"
   }, {
     quote: "Today, we fight for more than the Republic.  Today, we fight for all our brothers back home",
-    author: "Captain Rex"
+    author: "Captain Rex<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Rookies&#8221;"
   }, {
     quote: "Had you said the word, I would have left the Jedi Order",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Voyage of Temptation&#8221;"
   }, {
     quote: "You are the Chosen One.  You have brought balance to this world.  Stay on this path, and you will do it again for the galaxy.  But beware your heart...",
-    author: "The Father"
+    author: "The Father<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Ghosts of Mortis&#8221;"
   }, {
     quote: "We are loyal soldiers, we follow orders, but we are not a bunch of unthinking droids!  We are men!  We must be trusted to make the right decision, especially when the orders we are given are wrong",
-    author: "Fives"
+    author: "Fives<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Carnage of Krell&#8221;"
   }, {
     quote: "You can kill me, but you will never destroy me.  It takes strength to resist the dark side.  Only the weak embrace it",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;The Lawless&#8221;"
   }, {
     quote: "There is no mercy",
-    author: "Darth Sidious"
+    author: "Darth Sidious<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;The Lawless&#8221;"
   }, {
     quote: "My master abandoned me, and that's exactly what you've done to her.  You and your precious Jedi Order",
-    author: "Asajj Ventress"
+    author: "Asajj Ventress<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;The Wrong Jedi&#8221;"
   }, {
     quote: "The mission...the nightmares...they're finally over...",
-    author: "Fives"
+    author: "Fives<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Orders&#8221;"
   }, {
     quote: "No longer certain that one ever does win a war, I am.  For in fighting the battles, the bloodshed, already lost, we have",
-    author: "Yoda"
+    author: "Yoda<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Sacrifice&#8221;"
   }, {
     quote: "It is not the way of your people to hide here, in the gutters.  If you die, I promise you itt will be on the field of battle.  And if you die, you will die as warriors",
-    author: "Darth Maul"
+    author: "Darth Maul<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;The Phantom Apprentice&#8221;"
   }, {
-    quote: "Justice is merely a construct of the current power base",
-    author: "Darth Maul"
+    quote: "Justice is merely a construct of the current power base.  A base which, according to my calculations, is about to change",
+    author: "Darth Maul<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;The Phantom Apprentice&#8221;"
   }, {
     quote: "You're a good soldier, Rex.  So is every one of those men down there.  They may be willing to die, but I am not the one who is going to kill them",
-    author: "Ahsoka Tano"
+    author: "Ahsoka Tano<br><em>Star Wars:  The Clone Wars</em> &#8212; &#8220;Victory and Death&#8221;"
   }, {
     quote: "Anakin, if one is to understand the great mystery, one must study all its aspects, not just the dogmatic, narrow view of the Jedi.  If you wish to become a complete and wise leader, you must embrace a larger view of the Force",
-    author: "Darth Sidious"
+    author: "Darth Sidious<br><em>Star Wars:  Episode &#8546; &#8212; Revenge of the Sith</em>"
   }, {
     quote: "Once more, the Sith will rule the galaxy.  And we shall have peace",
-    author: "Darth Sidious"
+    author: "Darth Sidious<br><em>Star Wars:  Episode &#8546; &#8212; Revenge of the Sith</em>"
   }, {
     quote: "So this is how liberty dies:  with thunderous applause",
-    author: "Padme Amidala"
+    author: "Padme Amidala<br><em>Star Wars:  Episode &#8546; &#8212; Revenge of the Sith</em>"
   }, {
     quote: "You were my brother, Anakin!  I loved you",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Episode &#8546; &#8212; Revenge of the Sith</em>"
   }, {
     quote: "I was a coward.  But now, I know there's something stronger than fear, far stronger:  the Force.  Let me show you how strong it is",
-    author: "Kanan Jarrus"
+    author: "Kanan Jarrus<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Fire Across the Galaxy&#8221;"
   }, {
     quote: "Boys, this might be it.  At least we'll go down fighting like a clone should",
-    author: "Captain Rex"
+    author: "Captain Rex<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Relics of the Old Republic&#8221;"
   }, {
     quote: "I am no Jedi",
-    author: "Ahsoka Tano"
-  }, {
-    quote: "Battles leave scars.  Some you can't see",
-    author: "Kanan Jarrus"
+    author: "Ahsoka Tano<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Twilight of the Apprentice&#8221;"
   }, {
     quote: "To defeat an enemy, you must know them.  Not simply their battle tactics but their history, philosophy, art",
-    author: "Grand Admiral Thrawn"
+    author: "Grand Admiral Thrawn<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Hera's Heroes&#8221;"
+  }, {
+    quote: "Battles leave scars.  Some you can't see",
+    author: "Kanan Jarrus<br><em>Star Wars:  Rebels</em> &#8212; &#8220;The Last Battle&#8221;"
   }, {
     quote: "The Empire wanted to destroy worlds, and they did.  They destroyed mine",
-    author: "Sabine Wren"
+    author: "Sabine Wren<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Trials of the Darksaber&#8221;"
   }, {
     quote: "If you define yourself by the power to take life, the desire to dominate, to possess...then you have nothing",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Twin Suns&#8221;"
   }, {
     quote: "In my experience, when you think you understand the Force, you realize just how little you know",
-    author: "Ahsoka Tano"
+    author: "Ahsoka Tano<br><em>Star Wars:  Rebels</em> &#8212; &#8220;A World Between Worlds&#8221;"
   }, {
     quote: "Who deserves what is irrelevant.  What matters is who has power",
-    author: "Grand Admiral Thrawn"
+    author: "Grand Admiral Thrawn<br><em>Star Wars:  Rebels</em> &#8212; &#8220;Family Reunion and Farewell&#8221;"
   }, {
     quote: "We have hope.  Rebellions are built on hope",
-    author: "Jyn Erso"
+    author: "Jyn Erso<br><em>Rogue One:  A Star Wars Story</em>"
   }, {
     quote: "Help me, Obi-Wan Kenobi.  You're my only hope",
-    author: "Princess Leia Organa"
+    author: "Princess Leia Organa<br><em>Star Wars:  Episode &#8547; &#8212; A New Hope</em>"
   }, {
     quote: "For over a thousand generations, the Jedi Knights were the guardians of peace and justice in the Old Republic.  Before the dark times.  Before the Empire",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Episode &#8547; &#8212; A New Hope</em>"
   }, {
     quote: "Don't be too proud of this technological terror you've constructed.  The ability to destroy a planet is insignificant next to the power of the Force",
-    author: "Darth Vader"
+    author: "Darth Vader<br><em>Star Wars:  Episode &#8547; &#8212; A New Hope</em>"
   }, {
     quote: "You can't win, Darth.  If you strike me down, I shall become more powerful than you can possibly imagine",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Episode &#8547; &#8212; A New Hope</em>"
   }, {
     quote: "Remember:  the Force will be with you...always",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Episode &#8547; &#8212; A New Hope</em>"
   }, {
     quote: "Do, or do not.  There is no try",
-    author: "Yoda"
+    author: "Yoda<br><em>Star Wars:  Episode &#8548; &#8212; The Empire Strikes Back</em>"
+  }, {
+    quote: "If you only knew the power of the dark side",
+    author: "Darth Vader<br><em>Star Wars:  Episode &#8548; &#8212; The Empire Strikes Back</em>"
   }, {
     quote: "Luke, you're going to find that many of the truths we cling to depend greatly on our own point of view",
-    author: "Obi-Wan Kenobi"
+    author: "Obi-Wan Kenobi<br><em>Star Wars:  Episode &#8549; &#8212; Return of the Jedi</em>"
   }, {
     quote: "The Force is strong in my family.  My father has it...I have it...and my sister has it",
-    author: "Luke Skywalker"
+    author: "Luke Skywalker<br><em>Star Wars:  Episode &#8549; &#8212; Return of the Jedi</em>"
   }, {
     quote: "I am a Jedi...like my father before me",
-    author: "Luke Skywalker"
+    author: "Luke Skywalker<br><em>Star Wars:  Episode &#8549; &#8212; Return of the Jedi</em>"
   }, {
     quote: "I can bring you in warm, or I can bring you in cold",
-    author: "Din Djarin"
+    author: "Din Djarin<br><em>The Mandalorian</em> &#8212; &#8220;Chapter 1:  The Mandalorian&#8221;"
   }, {
     quote: "I'm a Mandalorian.  Weapons are part of my religion",
-    author: "Din Djarin"
+    author: "Din Djarin<br><em>The Mandalorian</em> &#8212; &#8220;Chapter 2:  The Child&#8221;"
   }, {
     quote: "Everybody's got their line they won't cross until things get messy.  As far as I'm concrened, if you can make it through your day and still sleep at night, you're doing better than most",
-    author: "Migs Mayfeld"
+    author: "Migs Mayfeld<br><em>The Mandalorian</em> &#8212; &#8220;Chapter 15:  The Believer&#8221;"
   }, {
     quote: "You have something I want.  You may think you have some idea of what you are in possession of, but you do not.  Soon, he will be back with me.  He means more to me than you will ever know",
-    author: "Din Djarin"
+    author: "Din Djarin<br><em>The Mandalorian</em> &#8212; &#8220;Chapter 15:  The Believer&#8221;"
   }, {
     quote: "He is strong with the Force, but talent without training is nothing",
-    author: "Luke Skywalker"
+    author: "Luke Skywalker<br><em>The Mandalorian</em> &#8212; &#8220;Chapter 16:  The Rescue&#8221;"
   }
 ];
 
